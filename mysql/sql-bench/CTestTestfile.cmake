93500019fea53b9707cb67d20b783df7f2f08f45
Tejaswi Sharma <tejaswi.sharma@meritnation.com>
2013-09-13 23:26:30 +0530

    Deleted all files and folder to create fresh

--- mysql/sql-bench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/local/src/mysql-5.5.28/sql-bench
-# Build directory: /usr/local/src/mysql-5.5.28/sql-bench
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
