8e8ee809c0504e8c96909d46e0ab0a0653893bdf
Tejaswi Sharma <tejaswi.sharma@meritnation.com>
2013-09-20 11:21:06 +0530

    Added first working setup for 2.x release

---
+++ lib/mysql/mysql-test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/local/src/mysql-5.5.28/mysql-test
+# Build directory: /usr/local/src/mysql-5.5.28/mysql-test
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
