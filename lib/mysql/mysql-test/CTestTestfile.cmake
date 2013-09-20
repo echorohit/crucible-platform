288079caf3d35015d28bac0e77d501efed9f1239
Tejaswi Sharma <tejaswi.sharma@meritnation.com>
2013-09-20 12:18:19 +0530

    deleted lib/mysql/mysql-test folder

--- lib/mysql/mysql-test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/local/src/mysql-5.5.28/mysql-test
-# Build directory: /usr/local/src/mysql-5.5.28/mysql-test
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
